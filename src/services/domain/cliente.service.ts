e76d602dc446d8863ef48c7c01a7c606f25ec808
David Magalhães <deivi_gf144@hotmail.com>
2022-01-27 21:31:07 -0400

    Tela de confirmacao de pedido

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -10,6 +10,11 @@ export class ClienteService {
 
     constructor(public http: HttpClient, public storage: StorageService){}
 
+
+    findById(id: string){
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
+    }
+
     findByEmail(email: string) {
         return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
