3a9b43fd253ff2b6808032a168380e7920d51357
David Magalhães <deivi_gf144@hotmail.com>
2022-01-28 21:34:00 -0400

    Infinite scroll

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -13,8 +13,8 @@ export class ProdutoService{
         return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
     }
 
-    findCategoriaById(categoria_id: string){
-        return this.http.get(`${API_CONFIG.baseUrl}/produtos?categorias=${categoria_id}`);
+    findCategoriaById(categoria_id: string, page: number = 0, linesPerPage: number = 24){
+        return this.http.get(`${API_CONFIG.baseUrl}/produtos?categorias=${categoria_id}&page=${page}&linesPerPage=${linesPerPage}`);
     }
 
 
